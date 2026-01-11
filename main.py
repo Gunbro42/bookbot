bbcfcaca496c7a838bac6703dad8f3b3251e38b8
gunbro42 <colinjustice29@gmail.com>
2026-01-10 21:34:58 -0600

    Bookbot now sorts and no longer requires hardcoded filepath the final commit for the bootdev bookbot

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+import sys
 from stats import word_count, get_char_counts
 
 def get_book_text(filepath):
@@ -6,7 +8,7 @@ def get_book_text(filepath):
 # Returns filepath as string
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
 
     # Beginning Bookbot
     print(f"============ BOOKBOT ============")
@@ -20,10 +22,28 @@ def main():
     num_words = word_count(text)
     print(f"Found {num_words} total words")
 
-    # Bookpath text character count tallied
-    characters = get_char_counts(text)
-    print(characters)
+    #Starting Character count
+    print(f"--------- Character Count -------")
 
+    # Bookpath text character count tallied in dictionary and sorted
+    characters = get_char_counts(text)
+    character_count_string = ""
+    character_count_keys = characters.keys()
+    for key in character_count_keys:
+        if not key.isalpha():
+            continue
+        character_count_string += key + ": " + str(characters[key]) + f'\n'
+    print(character_count_string, end="")
+    # Sys arguments
+if __name__== '__main__':
+    if not len(sys.argv) == 2:
+        print(f'Usage: python3 {sys.argv[0]} <path_to_book>')
+        sys.exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print("Error: File does not exist")
+        sys.exit(1)
+    # End of bookbot analysis
+    print(f"============= END ===============")
 main()
 
 
